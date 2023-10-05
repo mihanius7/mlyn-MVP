5a703627f5408ffb55e9b8a002c5969f54f171f4
Mihas Dainiak <mihanius@gmail.com>
2023-10-05 16:07:28 +0200

    New ViewportMouseListener inheritance classes

---
+++ src/gui/viewport/listeners/ViewportMouseListenerAct.java
@@ -0,0 +1,34 @@
+package gui.viewport.listeners;
+
+import java.awt.event.MouseEvent;
+
+import gui.MainWindow;
+import gui.viewport.Viewport;
+
+public class ViewportMouseListenerAct extends ViewportMouseListener{
+
+	public ViewportMouseListenerAct(Viewport v, MainWindow w) {
+		super(v, w);
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		super.mousePressed(arg0);
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		super.mouseDragged(arg0);
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		super.mouseReleased(arg0);
+	}
+
+	
+
+}
---
+++ src/gui/viewport/listeners/ViewportMouseListenerAdd.java
@@ -0,0 +1,32 @@
+package gui.viewport.listeners;
+
+import java.awt.event.MouseEvent;
+
+import gui.MainWindow;
+import gui.viewport.Viewport;
+
+public class ViewportMouseListenerAdd extends ViewportMouseListener {
+
+	public ViewportMouseListenerAdd(Viewport v, MainWindow w) {
+		super(v, w);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		super.mousePressed(arg0);
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		super.mouseDragged(arg0);
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		super.mouseReleased(arg0);
+	}
+}
